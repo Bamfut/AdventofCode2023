73b7b7932c0533938a6307b7f210d5f6328fa4db
Jakob Erber <jakoberber@sz-ybbs.ac.at>
2023-12-08 15:14:41 +0100

    day7 part 1

--- src/main/kotlin/day7/Main-1.kt
+++ src/main/kotlin/day7/Main-1.kt
@@ -11,14 +11,22 @@ fun main(args: Array<String>) {
     inputStream.bufferedReader().forEachLine { lineList.add(it) }
 
     val handMap = mutableMapOf<String, Int>()
+    var totalWinnings = 0
+    val allCards = listOf("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
 
-    lineList.forEach { it ->
+    lineList.forEach {
         handMap[it.split(" ")[0]] = it.split(" ")[1].toInt()
     }
 
-    /*lineList.forEach { it ->
-        println(it.split(" ")[0].groupBy { it })
-    }*/
+    val sortedMap = handMap.toList()
+        .sortedWith(compareBy({ getHand(it.first).value }, { allCards.indexOf(it.first[0].toString()) }, { allCards.indexOf(it.first[1].toString()) }, { allCards.indexOf(it.first[2].toString()) }, { allCards.indexOf(it.first[3].toString()) }, { allCards.indexOf(it.first[4].toString())}))
+        .toMap()
+
+    for (i in 0..<sortedMap.size) {
+        totalWinnings += sortedMap.toList()[i].second * (i + 1)
+    }
+
+    println("Total Winnings: $totalWinnings")
 }
 
 enum class Hand(val value: Int) {
@@ -29,4 +37,32 @@ enum class Hand(val value: Int) {
     TWO_PAIR(3),
     ONE_PAIR(2),
     HIGH_CARD(1)
+}
+
+fun getHand(stringHand: String): Hand {
+    if (stringHand.groupBy { it }.values.any { it.size == 5 }) {
+        return Hand.FIVE_OF_A_KIND
+    }
+
+    if (stringHand.groupBy { it }.values.any { it.size == 4 }) {
+        return Hand.FOUR_OF_A_KIND
+    }
+
+    if (stringHand.groupBy { it }.values.any { it.size == 3 } && stringHand.groupBy { it }.values.any { it.size == 2 }) {
+        return Hand.FULL_HOUSE
+    }
+
+    if (stringHand.groupBy { it }.values.any { it.size == 3 }) {
+        return Hand.THREE_OF_A_KIND
+    }
+
+    if (stringHand.groupBy { it }.values.filter { it.size == 2 }.size == 2) {
+        return Hand.TWO_PAIR
+    }
+
+    if (stringHand.groupBy { it }.values.any { it.size == 2 }) {
+        return Hand.ONE_PAIR
+    }
+
+    return Hand.HIGH_CARD
 }
